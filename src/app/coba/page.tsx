3741ad588c0b5dab6f6578757b66ae94073ab178
Oktavian Dwi Putra <oktavian.dwiputra@gmail.com>
2024-08-17 15:18:54 +0700

    Revise admin questions

--- src/app/coba/page.tsx
+++ src/app/coba/page.tsx
@@ -1,3 +1,8 @@
-(function () {
-  return typeof arguments;
-})();
+import React from "react";
+
+export default function page() {
+  (function () {
+    return typeof arguments;
+  })();
+  return <div>page</div>;
+}
