35fe98253d2e43a382b3048c79cd77176145f772
Oktavian Dwi Putra <oktavian.dwiputra@gmail.com>
2024-08-21 00:19:20 +0700

    Revise theme

--- src/app/quiz/[type]/page.tsx
+++ src/app/quiz/[type]/page.tsx
@@ -238,7 +238,7 @@ export default function Quiz({ params }: { params: { type: string } }) {
   return (
     <div className="space-y-4 mb-10">
       {/* Time Bar */}
-      <div className="relative w-full h-5 border-[2px] rounded-full">
+      <div className="relative w-full h-6 border-[2px] rounded-full">
         <div
           className={`bg-white h-4 rounded-full`}
           style={{ width: `${(timer / 60) * 100}%` }}
