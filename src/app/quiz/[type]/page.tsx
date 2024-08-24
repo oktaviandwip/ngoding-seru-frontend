670e4ba0caadd2deed465d360cea6e93a256a49a
Oktavian Dwi Putra <oktavian.dwiputra@gmail.com>
2024-08-24 13:43:02 +0700

    Add Score & Rank

--- src/app/quiz/[type]/page.tsx
+++ src/app/quiz/[type]/page.tsx
@@ -245,7 +245,7 @@ export default function Quiz({ params }: { params: { type: string } }) {
                 <div className="text-error">
                   Incorrect: {incorrectAnswersCount}
                 </div>
-                {/* <div className="text-blue">Score: {score}/100</div> */}
+                <div className="text-purple">% Correct: {score}%</div>
               </div>
 
               <Button
