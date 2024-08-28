0b68f3eaee230d7503d9ccb9402741cafd8df2e3
Oktavian Dwi Putra <oktavian.dwiputra@gmail.com>
2024-08-28 13:00:47 +0700

    Add total score & rank

--- src/app/api/auth/callback/google/page.tsx
+++ src/app/api/auth/callback/google/page.tsx
@@ -1,18 +1,14 @@
 // pages/api/auth/callback/google.ts
-import type { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const session = await getSession({ req });
+export default async function handler() {
+  const session = await getSession();
 
   if (session) {
     // Handle authenticated session
-    res.redirect("/");
+    console.log("sukses");
   } else {
     // Handle unauthenticated session
-    res.redirect("/login");
+    console.log("salah");
   }
 }
